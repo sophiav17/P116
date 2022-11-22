66ddd56c48b1fb40518ba46ddacada7c9f035601
Sophia <99284300+sophiav17@users.noreply.github.com>
2022-11-22 17:50:42 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    lipstick_link = loadImage('https://i.postimg.cc/85LtyBFn/l1.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    
+    if(results.length > 0);
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
